edfe4d206b8970f14e6a5982302dd45e7d5b4d11
Zeruelsito <122029741+Zeruelsito@users.noreply.github.com>
2025-06-24 19:21:05 -0300

    Boton resaltado Consigna 1

---
+++ js/resalta-boton.js
@@ -0,0 +1,12 @@
+function botonClickeado(numeroBoton) {
+    let botonesSelecciones = document.getElementsByClassName("tab-categoria")
+    let botonModificar = botonesSelecciones[numeroBoton].id
+
+    for (let i = 0; i < botonesSelecciones.length; i++) {
+        if (botonesSelecciones[i].id === botonModificar) {
+            document.getElementById(botonesSelecciones[i].id).style.cssText = 'background-color: black; color: white; border-radius:20px;';
+        } else {
+          document.getElementById(botonesSelecciones[i].id).style.cssText = 'background-color: white; color: dark;';
+        }
+    }
+}
