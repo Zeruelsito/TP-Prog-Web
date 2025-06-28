9db1adf09367cc9956cb122e5d6621282a6e5949
MarcosAlba <marquitosalba2018@gmail.com>
2025-06-28 17:24:48 -0300

    Boton arreglado con colores

--- js/resalta-boton.js
+++ js/resalta-boton.js
@@ -4,9 +4,9 @@ function botonClickeado(numeroBoton) {
 
     for (let i = 0; i < botonesSelecciones.length; i++) {
         if (botonesSelecciones[i].id === botonModificar) {
-            document.getElementById(botonesSelecciones[i].id).style.cssText = 'background-color: black; color: white; border-radius:20px;';
+            document.getElementById(botonesSelecciones[i].id).style.cssText = 'background-color: var(--color-texto); color: var(--color-fondo); border-radius:20px;';
         } else {
-            document.getElementById(botonesSelecciones[i].id).style.cssText = 'background-color: white; color: dark;';
+            document.getElementById(botonesSelecciones[i].id).style.cssText = 'color: var(--color-texto);';
         }
     }
 }
