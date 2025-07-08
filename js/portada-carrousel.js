b810b3735dfaa67e62eabdd1d23e5bba80e46e9b
MarcosAlba <marquitosalba2018@gmail.com>
2025-07-07 23:42:42 -0300

    TP Actualizado (Falta consigna de Favoritos)

--- js/portada-carrousel.js
+++ js/portada-carrousel.js
@@ -6,18 +6,53 @@ function numeroAleatorio() {
     return Math.floor(Math.random() * 60) + 1;
 }
 
-// Función para obtener una portada aleatoria
-function obtenerPortadaAleatoria() {
-    let portadaAleatoria;
+function obtenerCincoPortadasAleatorias() {
+    let indices = []; // Aca voy a guardar todos los numeros que van a salir con la funcion de NumeroAleatorio
 
-    //Acá deben agregar el código para obtener una portada aleatoria (portada de algún item),
-    //y guardarla en la variable portadaAleatoria
-
-    return portadaAleatoria;
+    while (indices.length < 5) {
+        let indice = numeroAleatorio() - 1; // Para que vaya de 0 a 59 porque los arrays empiezan en 0.
+        if (!indices.includes(indice)) {
+            indices.push(indice); // Verifico que el indice no se repita, y si no se repite lo agrego al array de indices
+        }
+    }
+    // De esta manera obtengo los 5 items usando esos indices
+    let portadas = indices.map(i => items[i]);
+    return portadas
 }
 
-// A continuación, deben agregar el código para "enlazar" 5 portadas aleatorias al carrusel
+const carrusel = document.getElementById('carouselExampleIndicators');
+const carouselInner = carrusel.querySelector('.carousel-inner'); // Asi selecciono solo la parte donde van los slides y no otro .carrousel-inner
+const carruselIndicators = carrusel.querySelector('.carousel-indicators');
 
-// Agarrar los elementos del dom del carrousel
-// Hacer parecido a lo de cargar items de la web
-// 
+//Limpio el HTMl para que no se acumulen slides
+carouselInner.innerHTML = '';
+carruselIndicators.innerHTML = '';
+
+const portadas = obtenerCincoPortadasAleatorias();
+
+portadas.forEach((item, i) => {
+    // Creo el slide
+    const slide = document.createElement('div');
+    slide.className = 'carousel-item' + (i === 0 ? ' active' : '');
+    slide.innerHTML = `
+      <div class="text-center">
+        <img src="${item.Foto_Carousel}" class="d-block w-100" alt="${item.Nombre}">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>${item.Nombre}</h5>
+      </div>
+    `;
+    carouselInner.appendChild(slide);
+
+    // Creo los botones para cambiar de slide
+    const indicator = document.createElement('button');
+    indicator.type = 'button';
+    indicator.setAttribute('data-bs-target', '#carouselExampleIndicators');
+    indicator.setAttribute('data-bs-slide-to', i);
+    indicator.setAttribute('aria-label', `Slide ${i + 1}`);
+
+    if (i === 0) {
+        indicator.className = 'active';
+        indicator.setAttribute('aria-current', 'true');
+    }
+    carruselIndicators.appendChild(indicator);
+})
